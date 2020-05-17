5ab08648cae5dca2b3cce42cfc04e44cc44d2fbd
pujanov69 <pujanov69@gmail.com>
2020-05-17 15:55:40 +0545

    gdx setup

---
+++ desktop/src/com/ishani/onelinepuzzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ishani.onelinepuzzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ishani.onelinepuzzle.OneLinePuzzle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OneLinePuzzle(), config);
+	}
+}
