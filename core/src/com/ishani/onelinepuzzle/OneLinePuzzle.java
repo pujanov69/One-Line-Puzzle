d98981c9aa69769a77feb361018b068645dbad0f
pujanov69 <pujanov69@gmail.com>
2020-05-20 23:03:03 +0545

    Starfish Collector chap3 implemented

--- core/src/com/ishani/onelinepuzzle/OneLinePuzzle.java
+++ core/src/com/ishani/onelinepuzzle/OneLinePuzzle.java
@@ -1,33 +1,52 @@
 package com.ishani.onelinepuzzle;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.ishani.onelinepuzzle.actors.BaseActor;
+import com.ishani.onelinepuzzle.actors.Starfish;
+import com.ishani.onelinepuzzle.actors.Turtle;
+import com.ishani.onelinepuzzle.actors.Whirlpool;
 
-public class OneLinePuzzle extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class OneLinePuzzle extends GameBeta {
+
+	private Turtle turtle;
+	private Starfish starfish;
+	private BaseActor ocean;
+
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void initialize() {
+		ocean = new BaseActor(0,0, mainStage);
+		ocean.loadTexture("water.jpg");
+		ocean.setSize(800, 600);
+
+		starfish = new Starfish(380,380, mainStage);
+
+		turtle = new Turtle(20,20,mainStage);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void update(float dt) {
+		if (turtle.overlaps(starfish) && !starfish.isCollected() ) {
+			starfish.collect();
+			Whirlpool whirl = new Whirlpool(0,0, mainStage);
+			whirl.centerAtActor( starfish );
+			whirl.setOpacity(0.25f);
+			BaseActor youWinMessage = new BaseActor(0,0,mainStage);
+			youWinMessage.loadTexture("you-win.png");
+			youWinMessage.centerAtPosition(400,300);
+			youWinMessage.setOpacity(0);
+			youWinMessage.addAction( Actions.delay(1) );
+			youWinMessage.addAction( Actions.after( Actions.fadeIn(1) ) );
+		}
+
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
---
+++ core/src/com/ishani/onelinepuzzle/actors/Starfish.java
@@ -0,0 +1,34 @@
+package com.ishani.onelinepuzzle.actors;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+
+public class Starfish extends BaseActor {
+
+    private boolean collected;
+
+    public Starfish(float x, float y, Stage s) {
+        super(x, y, s);
+
+        loadTexture("starfish.png");
+
+        setBoundaryPolygon(8);
+
+        Action spin = Actions.rotateBy(30,1);
+        this.addAction(Actions.forever(spin));
+
+        collected = false;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+    public void collect() {
+        collected = true;
+        clearActions();
+        addAction( Actions.fadeOut(1) );
+        addAction( Actions.after( Actions.removeActor() ) );
+    }
+
+}
